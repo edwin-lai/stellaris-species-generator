74dc4e75570332941595fa69c90a80ecd72d80f0
Brian Lambert <Brian.joseth.lambert@gmail.com>
2016-04-05 13:14:08 -0700

    Finish ethos symbol placement

--- lib/assets/components/ethos.jsx
+++ lib/assets/components/ethos.jsx
@@ -6,22 +6,22 @@ var Ethos = React.createClass({
   render: function() {
     return (
       <div className="ethos-holder">
-        <div className="ethos fanatic-militarist" />
-        <div className="ethos militarist" />
-        <div className="ethos fanatic-spiritualist" />
-        <div className="ethos spiritualist" />
-        <div className="ethos fanatic-xenophobe" />
-        <div className="ethos xenophobe" />
-        <div className="ethos fanatic-collectivist" />
-        <div className="ethos collectivist" />
-        <div className="ethos fanatic-individualist" />
-        <div className="ethos individualist" />
-        <div className="ethos fanatic-xenophile" />
-        <div className="ethos xenophile" />
-        <div className="ethos fanatic-materialist" />
-        <div className="ethos materialist" />
-        <div className="ethos fanatic-pacifist" />
-        <div className="ethos pacifist" />
+        <div className="ethos fanatic-militarist" id="fanatic-militarist" />
+        <div className="ethos militarist" id="militarist" />
+        <div className="ethos fanatic-spiritualist" id="fanatic-spiritualist" />
+        <div className="ethos spiritualist" id="spiritualist" />
+        <div className="ethos fanatic-xenophobe" id="fanatic-xenophobe" />
+        <div className="ethos xenophobe" id="xenophobe" />
+        <div className="ethos fanatic-collectivist" id="fanatic-collectivist" />
+        <div className="ethos collectivist" id="collectivist" />
+        <div className="ethos fanatic-individualist" id="fanatic-individualist" />
+        <div className="ethos individualist" id="individualist" />
+        <div className="ethos fanatic-xenophile" id="fanatic-xenophile" />
+        <div className="ethos xenophile" id="xenophile" />
+        <div className="ethos fanatic-materialist" id="fanatic-materialist" />
+        <div className="ethos materialist" id="materialist" />
+        <div className="ethos fanatic-pacifist" id="fanatic-pacifist" />
+        <div className="ethos pacifist" id="pacifist" />
       </div>
     );
   }
