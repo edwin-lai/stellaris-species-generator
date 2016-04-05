cac870e7c89891242a6a2f6640dbe097e2b1036a
Brian Lambert <Brian.joseth.lambert@gmail.com>
2016-04-05 14:58:35 -0700

    Basic styles for other ethos components

--- lib/assets/components/ethos.jsx
+++ lib/assets/components/ethos.jsx
@@ -2,27 +2,37 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var Ethos = React.createClass({
+  getInitialState: function () {
+    return ({
+      ethicsPoints: 3
+    });
+  },
+
 
   render: function() {
     return (
-      <div className="ethos-holder">
-        <div className="ethos fanatic-militarist" id="fanatic-militarist" />
-        <div className="ethos militarist" id="militarist" />
-        <div className="ethos fanatic-spiritualist" id="fanatic-spiritualist" />
-        <div className="ethos spiritualist" id="spiritualist" />
-        <div className="ethos fanatic-xenophobe" id="fanatic-xenophobe" />
-        <div className="ethos xenophobe" id="xenophobe" />
-        <div className="ethos fanatic-collectivist" id="fanatic-collectivist" />
-        <div className="ethos collectivist" id="collectivist" />
-        <div className="ethos fanatic-individualist" id="fanatic-individualist" />
-        <div className="ethos individualist" id="individualist" />
-        <div className="ethos fanatic-xenophile" id="fanatic-xenophile" />
-        <div className="ethos xenophile" id="xenophile" />
-        <div className="ethos fanatic-materialist" id="fanatic-materialist" />
-        <div className="ethos materialist" id="materialist" />
-        <div className="ethos fanatic-pacifist" id="fanatic-pacifist" />
-        <div className="ethos pacifist" id="pacifist" />
-      </div>
+      <section className="ethos-wrapper">
+        <div className="ethos-holder">
+          <div className="ethos fanatic-militarist" id="fanatic-militarist" />
+          <div className="ethos militarist" id="militarist" />
+          <div className="ethos fanatic-spiritualist" id="fanatic-spiritualist" />
+          <div className="ethos spiritualist" id="spiritualist" />
+          <div className="ethos fanatic-xenophobe" id="fanatic-xenophobe" />
+          <div className="ethos xenophobe" id="xenophobe" />
+          <div className="ethos fanatic-collectivist" id="fanatic-collectivist" />
+          <div className="ethos collectivist" id="collectivist" />
+          <div className="ethos fanatic-individualist" id="fanatic-individualist" />
+          <div className="ethos individualist" id="individualist" />
+          <div className="ethos fanatic-xenophile" id="fanatic-xenophile" />
+          <div className="ethos xenophile" id="xenophile" />
+          <div className="ethos fanatic-materialist" id="fanatic-materialist" />
+          <div className="ethos materialist" id="materialist" />
+          <div className="ethos fanatic-pacifist" id="fanatic-pacifist" />
+          <div className="ethos pacifist" id="pacifist" />
+        </div>
+        <h3 className="ethos-points">Ethic Points Left: {this.state.ethicsPoints}</h3>
+        <button className="reset-ethos">Cancel</button>
+      </section>
     );
   }
 
