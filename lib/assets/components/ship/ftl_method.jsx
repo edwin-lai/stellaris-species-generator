46f3d90c6843f3cb6d38f5b0b5b55de2fa263650
Justin Tung <JTung1104@gmail.com>
2016-04-28 18:58:52 -0400

    add styles to ftl methods and weapons

--- lib/assets/components/ship/ftl_method.jsx
+++ lib/assets/components/ship/ftl_method.jsx
@@ -4,12 +4,14 @@ var FTLMethod = React.createClass({
   render: function () {
     return (
       <div className="ftl">
-        <img src={this.props.imageUrl}/>
-        <div>
-          <h3>{this.props.name}</h3>
-          <h3>{this.props.recommendation}</h3>
-          <p>{this.props.description}</p>
-        </div>
+        <a href="#/ftlMethods">
+          <img src={this.props.imageUrl}/>
+          <div>
+            <h3>{this.props.name}</h3>
+            <h3 className="recommendation">{this.props.recommendation}</h3>
+            <p>{this.props.description}</p>
+          </div>
+        </a>
       </div>
     );
   }
